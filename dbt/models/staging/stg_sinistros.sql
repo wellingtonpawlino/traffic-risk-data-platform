bc39cf1bcc54e439b5c963b994a6f57bd769553a
Wellington Pawlino <wellingtonpawlino@gmail.com>
2026-05-16 14:56:54 -0300

    staging veiculos finalizada

--- dbt/models/staging/stg_sinistros.sql
+++ dbt/models/staging/stg_sinistros.sql
@@ -1,7 +1,19 @@
+
 SELECT
-    id_sinistro,
-    latitude,
-    longitude,
-    data_sinistro
-FROM  analytics.prep.sinistros_enriched
-WHERE latitude IS NOT NULL AND longitude IS NOT NULL
+     id_sinistro::int                                           AS id_sinistro
+	 ,id_veiculo::int                                            AS id_veiculo
+	 ,CASE
+    	WHEN marca_modelo IS NULL OR TRIM(marca_modelo) = '' OR LOWER(TRIM(marca_modelo)) IN ('nao informado', 'nao info', '-')
+    	THEN 'Sem Informacoes'
+    	ELSE TRIM(UPPER(marca_modelo)) END AS ds_marca_modelo
+	,CASE
+    	WHEN tipo_veiculo IS NULL OR TRIM(tipo_veiculo) = '' OR LOWER(TRIM(tipo_veiculo)) = 'nao disponivel'
+    	THEN 'Sem Informacoes'
+    	ELSE INITCAP(TRIM(LOWER(tipo_veiculo))) END AS ds_tipo_veiculo
+	,TO_DATE(NULLIF(data_sinistro, ''), 'DD/MM/YYYY')               AS dt_sinistro	
+
+	,ano_sinistro::int                                              AS ano_sinistro
+    ,mes_sinistro::int                                              AS mes_sinistro
+    ,dia_sinistro::int                                              AS dia_sinistro
+	 FROM prep.veiculos
+WHERE id_sinistro IS NOT NULL 
