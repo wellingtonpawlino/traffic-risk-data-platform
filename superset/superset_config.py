21bb3044fe109d8abdcb8e2f7eb1962c5a7a4e53
Wellington Pawlino <wellingtonpawlino@gmail.com>
2026-06-01 22:58:05 -0300

    ajuste imagem

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -1,1 +1,10 @@
 SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://airflow:airflow@postgres:5432/superset"
+
+
+# 🟢 habilita templates / features
+FEATURE_FLAGS = {
+    "ENABLE_TEMPLATE_PROCESSING": True
+}
+
+# 🟢 habilita tema via UI
+ENABLE_UI_THEME_ADMINISTRATION = True
